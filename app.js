3a72f4e52befe670a091fef7c17bd41b1ed2cfa8
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-03-28 11:47:52 +0430

    add updates

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-let button = document.getElementById("btn");
-let restart = document.getElementById("restart");
-let qrImg = document.getElementById("qrImg");
-let allPage = document.getElementById("all");
-let img = document.getElementById("img");
-
-
-button.addEventListener("click", ()=>{
-    let input = document.getElementById("input").value;
-    allPage.style.display = "none";
-    qrImg.style.display = "flex";
-    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${input}`;
+const button = document.querySelector(".submitBtn");
+const homeBtn = document.querySelector(".home");
+const restart = document.getElementById("restart");
+let input = document.getElementById("input");
+let imagePage = document.querySelector(".imagePage");
+let generatePage = document.querySelector(".generate");
+let data = [];
+let qrData = JSON.parse(localStorage.getItem("QrCode"));
+button.addEventListener("click", () => {
+  imagePage.innerHTML = "";
+  if (input.value != "") {
+    const qrCodSrc = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${input.value}`;
+    imagePage.style.display = "flex";
+    generatePage.style.display = "none";
+    homeBtn.style.display = "block";
+    data.push({
+      qrCodSrc: qrCodSrc,
+    });
+    localStorage.setItem("QrCode", JSON.stringify(data));
+    setData();
+  } else {
+    alert("input empty");
+  }
 });
-restart.addEventListener("click",()=>{
-    allPage.style.display = "flex";
-    qrImg.style.display = "none";
-    document.getElementById("input").value = "";
+restart.addEventListener("click", () => {
+  localStorage.removeItem("QrCode");
+  location.reload();
+});
 
-
-})
+homeBtn.addEventListener("click", () => {
+  imagePage.style.display = "none";
+  generatePage.style.display = "flex";
+  homeBtn.style.display = "none";
+});
+function setData() {
+  let qrData = JSON.parse(localStorage.getItem("QrCode"));
+  qrData.map((qr) => {
+    imagePage.innerHTML += `<img src=${qr.qrCodSrc} alt="">`;
+  });
+}
